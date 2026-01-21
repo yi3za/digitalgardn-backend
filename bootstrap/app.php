862020afea1da7cf19f3571925a8007db897765d
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-01-21 12:32:28 +0100

    Mise en place de l'authentification via Sanctum
    Configuration de Laravel Sanctum pour l'authentification stateful (cookie). Cela inclut la mise a jour du middleware, la configuration de CORS et l'ajout des domaines dans le fichier .env.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Utilise l'authentification par cookie de sanctum
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
