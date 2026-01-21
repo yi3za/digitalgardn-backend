3862ec8c114b9cd51ef11c3c0df2dddabce318fd
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-01-21 18:19:37 +0100

    Clarification de la configuration de CORS via des commantaires

--- config/cors.php
+++ config/cors.php
@@ -1,5 +1,17 @@
 <?php
 
+#-----------------------------------------------------------------------------------------
+# Configuration de CORS (Cross Origin Resource Sharing) pour l'authentification Stateful
+#-----------------------------------------------------------------------------------------
+#
+# 1. 'allowed_origins': On autorise explicitement notre application frontend
+#    (React, tournant sur http://localhost:3000 ) a envoyer des requetes.
+#
+# 2. 'supports_credentials': On met cette valeur a 'true' pour permettre
+#    l'echange de cookies entre le frontend et le backend. C'est essentiel
+#    pour que l'authentification par session de Sanctum fonctionne.
+#
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
