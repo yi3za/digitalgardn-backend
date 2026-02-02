68c8fb2eecdff1f329dd31e68574e36a9ea08be6
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-02-02 16:27:47 +0100

    fix(Security): Renforcer l'autorisation dans le Profil UpdateRequest

--- app/Http/Requests/Freelance/Profil/UpdateRequest.php
+++ app/Http/Requests/Freelance/Profil/UpdateRequest.php
@@ -14,7 +14,7 @@ class UpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return $this->user() !== null;
+        return $this->user() !== null  && $this->user()->role === 'freelance';
     }
 
     /**
