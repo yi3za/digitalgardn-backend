232d6b3a848786f26927c977c639e288ac7997aa
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-31 11:19:12 +0100

    fix(auth): configurer les domaines stateful و cors pour la gestion des sessions spa

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@
 #-----------------------------------------------------------------------------------------
 #
 # 1. 'allowed_origins': On autorise explicitement notre application frontend
-#    (React, tournant sur http://localhost:3000 ) a envoyer des requetes.
+#    a envoyer des requetes.
 #
 # 2. 'supports_credentials': On met cette valeur a 'true' pour permettre
 #    l'echange de cookies entre le frontend et le backend. C'est essentiel
@@ -15,7 +15,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [env('FRONTEND_URL')],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
