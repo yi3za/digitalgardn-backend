88cca003b692ec578c7cae4d5c6694ed1d9fd14a
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-04-23 15:05:14 +0100

    feat(resource): ajoute infos de la commande dans ConversationResource

--- app/Http/Resources/ConversationResource.php
+++ app/Http/Resources/ConversationResource.php
@@ -40,6 +40,15 @@ class ConversationResource extends JsonResource
             'latest_message' => $this->whenLoaded('latestMessage', function () {
                 return new MessageResource($this->latestMessage);
             }),
+            'commande' => $this->whenLoaded('commande', function () {
+                return [
+                    'id' => $this->commande?->id,
+                    'statut' => $this->commande?->statut,
+                    'client_id' => $this->commande?->client_id,
+                    'freelance_id' => $this->commande?->freelance_id,
+                    'montant' => $this->commande?->montant,
+                ];
+            }),
             'last_message_at' => $this->last_message_at,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
