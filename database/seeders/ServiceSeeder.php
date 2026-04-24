0f3f37d18802a63239463f5016cf83bf781dfa36
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-04-24 18:39:36 +0100

    chore(seeder): augmente nombre services crees par freelance

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -17,7 +17,7 @@ class ServiceSeeder extends Seeder
     {
         $freelances = User::where('role', 'freelance')->get();
         foreach ($freelances as $freelance) {
-            $count = rand(1, 20);
+            $count = rand(20, 50);
             $services = Service::factory($count)->create(['user_id' => $freelance->id]);
             foreach ($services as $service) {
                 $categoriesIds = Categorie::whereNotNull('parent_id')->inRandomOrder()->take(rand(1, 10))->pluck('id')->toArray();
